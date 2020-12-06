91dfce2f63919a5d69179d4cc404607651a8d277
jian-Nan <499103237@qq.com>
2020-12-06 17:41:44 +0800

    内容初始化配置

--- src/main/java/com/huangjn/blogbackstage/modules/content/controller/ContentController.java
+++ src/main/java/com/huangjn/blogbackstage/modules/content/controller/ContentController.java
@@ -1,10 +1,42 @@
 package com.huangjn.blogbackstage.modules.content.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/content")
 public class ContentController {
+    @GetMapping("/knowledge")
+    public String knowledge()
+    {
+        return "index";
+    }
 
+    /*
+     *127.0.0.1/account/music
+     */
+    @GetMapping("/music")
+    public String music()
+    {
+        return "index";
+    }
+
+    /*
+     *127.0.0.1/account/member
+     */
+    @GetMapping("/picture")
+    public String picture()
+    {
+        return "index";
+    }
+
+    @GetMapping("/software")
+    public String software()
+    {
+        return "index";
+    }
+    /*
+    }
+     */
 }
